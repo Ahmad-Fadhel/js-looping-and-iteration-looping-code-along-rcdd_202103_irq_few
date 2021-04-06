6aaa5bfb868de09a5f41e9edbfca4c9f84b7dc32
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-04-06 15:09:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ function writeCards(array, event){
   const thankYouMessage = [];
   let thankYou ;
   for (let i = 0; i < array.length; i++){
+<<<<<<< HEAD
        thankYou = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
       thankYouMessage.push(thankYou);
+=======
+       thankYou = `Thank you, ${array[i]} for the wonderful ${event} gift!`;
+      thankYouMessage.push(thankYou);
+      console.log(thankYou);
+>>>>>>> db67f566d246ed01392e3c019fc99c6efd10d806
   }
   return thankYouMessage;
   
@@ -21,5 +27,9 @@ function countDown(countDown){
 }
 
 
+<<<<<<< HEAD
 writeCards(["Lisa", "Kaitlin", "Jan"], 'surprise');
+=======
+writeCards(["ahmad,", "dovakin,", "akatosh,", "annie,"], 'surprise');
+>>>>>>> db67f566d246ed01392e3c019fc99c6efd10d806
 countDown(10);
